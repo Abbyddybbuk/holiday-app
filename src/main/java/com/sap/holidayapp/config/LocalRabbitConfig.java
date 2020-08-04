d2986d97ae75b2f3561396ad70b964a1a7c1743d
I066841 <a.kulshreshtha@sap.com>
2020-08-05 00:23:24 +0530

    RabbitMQ Configuration

---
+++ src/main/java/com/sap/holidayapp/config/LocalRabbitConfig.java
@@ -0,0 +1,56 @@
+package com.sap.holidayapp.config;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory.ConfirmType;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//import com.rabbitmq.client.ConnectionFactory;
+
+@Configuration
+@Profile({ "default" })
+public class LocalRabbitConfig {
+	public static final String HOLIDAY_QUEUE_NAME = "HOLIDAY_REQUEST";
+
+	@Bean
+	public ConnectionFactory rabbitMQConnectionFactory(@Value("${spring.rabbitmq.host}") String host,
+			@Value("${spring.rabbitmq.port}") int port, @Value("${spring.rabbitmq.username}") String username,
+			@Value("${spring.rabbitmq.password}") String password) {
+		CachingConnectionFactory factory = this.getConnectionFactory(host, port, username, password);
+		factory.setPublisherConfirmType(ConfirmType.NONE);
+		factory.setPublisherReturns(true);
+		factory.setChannelCacheSize(100);
+		return factory;
+	}
+
+	@Bean
+	public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+		RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+		rabbitAdmin.declareQueue(new Queue(LocalRabbitConfig.HOLIDAY_QUEUE_NAME));
+		return rabbitAdmin;
+	}
+
+	@Bean
+	public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMandatory(true);
+		return rabbitTemplate;
+	}
+
+	private CachingConnectionFactory getConnectionFactory(String host, int port, String username, String password) {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+		cachingConnectionFactory.setHost(host);
+		cachingConnectionFactory.setPort(port);
+		cachingConnectionFactory.setUsername(username);
+		cachingConnectionFactory.setPassword(password);
+		return cachingConnectionFactory;
+	}
+}
