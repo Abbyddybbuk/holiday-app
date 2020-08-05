514b426b90feadd6c211d8fb6b343a79c72f8ddf
I066841 <a.kulshreshtha@sap.com>
2020-08-05 23:11:27 +0530

    RabbitMQ Config

--- src/main/java/com/sap/holidayapp/config/LocalRabbitConfig.java
+++ src/main/java/com/sap/holidayapp/config/LocalRabbitConfig.java
@@ -13,8 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-//import com.rabbitmq.client.ConnectionFactory;
-
 @Configuration
 @Profile({ "default" })
 public class LocalRabbitConfig {
